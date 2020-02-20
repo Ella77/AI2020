512f09a7fc5d7f23b208a60ea04dc55a089b3d1f
bor1bori <bor1boridv@gmail.com>
2020-02-20 10:27:08 +0900

    hash 함수 추가

--- back/src/utils/auth.ts
+++ back/src/utils/auth.ts
@@ -11,3 +11,8 @@ export const jwtSign = (obj: any): string => {
 export const jwtVerify = (token: string): string | any => {
   return jwt.verify(token, privateKey);
 };
+
+// crypt
+export const hash = (plainText: string): string  => {
+  return crypto.createHash('sha512').update(plainText).digest('base64');
+};

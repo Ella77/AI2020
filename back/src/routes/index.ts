03c2351757fcabdc2b8eabec1bc6bb3e42d9bb6e
bor1bori <bor1boridv@gmail.com>
2020-02-20 11:20:01 +0900

    Signin SignUp 기능 완료: 컨트롤러 & 라우터 작성

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import meetingRouter from './meeting';
+import authRouter from './auth';
 
 const Router = express.Router();
+
+Router.use('/meetings', meetingRouter);
+Router.use('/auth', authRouter);
 
 Router.use('/', (req, res) => {
   res.status(200).send('hello');
 });
-
-Router.use('/meetings', meetingRouter);
 
 export default Router;

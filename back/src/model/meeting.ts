fa1edc6ddd945f6949af99fdde4d1f9a5878bda4
bor1bori <bor1boridv@gmail.com>
2020-02-20 10:22:12 +0900

    User 모델 & Meeting 모델 작성

--- back/src/model/meeting.ts
+++ back/src/model/meeting.ts
@@ -1,19 +1,45 @@
 import mongoose from 'mongoose';
 import { ObjectId } from 'bson';
+import { User } from './user';
+
+export type Record = {
+  userId: ObjectId | User;
+  sentence: string;
+}
 
 export type Agenda = {
   name: string;
-  records: string[];
+  
+  expectedTime: number; // ms
+  usedTime: number; // ms
+
+  startDate: Date;
+  endDate: Date | null;
+  
+  records: Record[];
 };
 
 export interface Meeting extends mongoose.Document {
+  sequenceNumberOfCurrentAgenda: number; // 현재 논의 중인 의제, 0부터 시작
   agendas: Agenda[];
+  participants: ObjectId[] | User[];
 }
 
 const schema = new mongoose.Schema({
+  sequenceNumberOfCurrentAgenda: {
+    default: 0,
+    type: Number
+  },
   agendas: {
     default: [],
     type: [mongoose.Schema.Types.Mixed]
+  },
+  participants: {
+    default: [],
+    type: [{
+      type: ObjectId,
+      ref: 'User'
+    }]
   }
 }, {
   timestamps: true
